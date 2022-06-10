2e05b9b71bdf2a3e9ba877222967baa6aedead9e
Sae <winny9641@gmail.com>
2022-06-10 03:16:20 -0400

    inputs implements

--- snake-client/input.js
+++ snake-client/input.js
@@ -1,10 +1,13 @@
 const { stdin } = require("process");
+let connection;
 
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on("data",handleUserInput);
   return stdin;
 };
 
@@ -12,9 +15,15 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+  if (data === "h") {
+    connection.write("Say: hello");
+  }
+  if (data === "m") {
+    connection.write("Say: move!");
+  }
 };
 
-stdin.on("data",handleUserInput);
+
 
 
 module.exports = {setupInput};
