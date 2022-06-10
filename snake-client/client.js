efb25883c36e6371e2684a626cd33e974164925e
Sae <winny9641@gmail.com>
2022-06-10 02:55:58 -0400

    seperate file into inputs.js

--- snake-client/client.js
+++ snake-client/client.js
@@ -1,3 +1,33 @@
-const {net,connect} = require("./play");
-net;
-connect;
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: "50541"
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("hello i entered");
+  });
+
+  conn.on("connect", ()=> {
+    conn.write("Name: SJ");
+  });
+  
+  
+  // conn.on("connect", ()=> {
+  //   setTimeout(()=>{
+  //     console.log("move up");
+  //     conn.write("Move: up");
+  //   },2000);
+    
+  // });
+ 
+
+  return conn;
+};
+
+module.exports = {connect};
